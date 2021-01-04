60e84989e86c058486350640b12ed60eedf2a0d3
Hieu Phan <64969360+Pogchop@users.noreply.github.com>
2021-01-04 09:57:07 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3ex1
+{
+    class Program
+    {
+        void printmessage(string message = "hello!")
+        {
+            Console.WriteLine("{0}", message);
+        }
+        static void Main(string[] args)
+        {
+            Program opexample = new Program();
+            opexample.printmessage("Welcome");
+            opexample.printmessage();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentCW
+{
+    class lol
+    {
+        int id;
+        string name;
+        DateTime dob;
+        float gpa;
+        public void Read()
+        {
+            Console.Write("Student ID: ");
+            id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Student name: ");
+            name = Console.ReadLine();
+            Console.Write("GPA: ");
+            gpa = float.Parse(Console.ReadLine());
+            Console.Write("Datetime: ");
+            dob = Convert.ToDateTime(Console.ReadLine());
+        }
+        public void Display()
+        {
+            int age = (DateTime.Today.Subtract(dob)).Days / 365;
+            Console.WriteLine("Student name: " + name);
+            Console.WriteLine("Student id: " + id);
+            Console.WriteLine("GPA: " + gpa);
+            Console.WriteLine("Age: " + age);
+            Console.ReadKey();
+        }
+    }
+    class Student { 
+        static void Main(string[] args)
+        {
+            lol lasa = new lol();
+            lasa.Read();
+            lasa.Display();
+            
+        }
+    }
+}
